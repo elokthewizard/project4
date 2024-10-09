57d64baadced1f12b9e21cd34f1d92c2f0563d32
elokthewizard <bradencbateman@gmail.com>
2024-10-08 19:00:05 -0600

    init models, makemigrations, and migrate

--- network/models.py
+++ network/models.py
@@ -1,6 +1,23 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.forms import ModelForm
 
 
 class User(AbstractUser):
-    pass
+    bio = models.CharField(max_length=128, blank=True)
+    following = models.ManyToManyField('self', symmetrical=False, related_name='following_users')
+    followed_by = models.ManyToManyField('self', symmetrical=False, related_name='followed_by_users')
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField(max_length=128)
+    time = models.DateTimeField(auto_now_add=True)
+    liked_by = models.ManyToManyField(User, blank=True, related_name="liked_by")
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content[:20]}'
+    
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        exclude = ["liked_by"]
